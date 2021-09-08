536f077bbe6a5c64bdb2e49f8e815f6cbfa7f0f5
shaunvan119 <shaunvan@live.com.au>
2021-09-08 21:25:19 +1000

    SlideTL functions added

--- app.js
+++ app.js
@@ -13,7 +13,14 @@ function animateSlides() {
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
     //GSAP
-
+     const slideTl = gsap.timeline({
+         defaults: { duration: 1, ease: "power2.inOut" }
+    });
+    slideTl.fromTo(revealImg, { x: "0%" }, { x: "100%"});
+    slideTl.fromTo(img, { scale: 2}, { scale: 1}, "-=1");
+    slideTl.fromTo(revealText, {x: "0%"}, {x: "100%"}, "-=0.75");
+    slideTl.fromTo(nav,{ y:"-100%"}, { y:"0%"}, "-=0.5" );
     });
 }
 
+animateSlides();
