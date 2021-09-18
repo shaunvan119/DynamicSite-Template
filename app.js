af3462b8af310e225f5492c191d14bac38fe5eb0
shaunvan119 <shaunvan@live.com.au>
2021-09-18 11:36:25 +1000

    page transition

--- app.js
+++ app.js
@@ -87,9 +87,21 @@ function activeCursor(e){
 }
 
 function navToggle(e) {
+    if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
     gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
     gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to("#logo", 1, {color: "black" });
     gsap.to(".nav-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+}else{
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to("#logo", 1, {color: "white" });
+    gsap.to(".nav-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+}
 }
 //Event Listener
 burger.addEventListener("click", navToggle);
